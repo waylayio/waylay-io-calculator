e30bba4c716711b52b21368162de36629aefec36
pizuricv <veselin.pizurica@gmail.com>
2021-03-18 15:14:19 +0100

    added pricing

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,10 +49,10 @@
 	});
 
 	$(function() {
-
-		var $window = $(window),
+		 var $window = $(window),
 			$body = $('body'),
 			$header = $('#header');
+			$("#pricingInfo").load("pricing.html"); 
 
 		// Disable animations/transitions until the page has loaded.
 			$body.addClass('is-loading');
